c098378745cc120f5c18cac06ab6ddc8b2527c17
Maksim Roi <klinarr@yandex.ru>
2024-09-24 21:00:07 +0300

    Поправил ошибки

--- js/functions.js
+++ js/functions.js
@@ -5,12 +5,23 @@ function lengthCheck (string, len) {
   return false;
 }
 
+lengthCheck('проверяемая строка', 20);
+lengthCheck('проверяемая строка', 18);
+lengthCheck('проверяемая строка', 10);
+
+
 function palidromeCheck (string){
   string = string.replaceAll(' ','').toLowerCase();
   return string === string.split('').reverse().join('');
 }
 
+palidromeCheck ('топот');
+palidromeCheck ('ДовОд');
+palidromeCheck ('Кекс');
+palidromeCheck ('Лёша на полке клопа нашёл ');
+
 function findDigit (string) {
+  string += '';
   let result = '';
 
   for (let i = 0; i < string.length; i++) {
@@ -18,5 +29,14 @@ function findDigit (string) {
       result += string[i];
     }
   }
-  return result;
+  return +result;
 }
+
+findDigit ('2023 год');
+findDigit ('ECMAScript 2022');
+findDigit ('1 кефир, 0.5 батона');
+findDigit ('агент 007');
+findDigit ('а я томат');
+findDigit (2023);
+findDigit (-1);
+findDigit (1.5);
